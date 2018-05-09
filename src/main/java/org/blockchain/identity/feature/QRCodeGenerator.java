dbf4a508ae01fd1de2ba98546dc221f0d1ffb8e8
ahuni01 <ahuni01@ca.com>
2018-05-09 10:21:23 +0530

    Initial commit

---
+++ src/main/java/org/blockchain/identity/feature/QRCodeGenerator.java
@@ -0,0 +1,28 @@
+package org.blockchain.identity.feature;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+public class QRCodeGenerator {
+
+    public static String generateQRCodeImage(String text) {
+        try {
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 150, 150);
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+            return Base64.getEncoder().encodeToString(outputStream.toByteArray());
+        } catch (WriterException | IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
